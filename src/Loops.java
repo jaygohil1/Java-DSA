ebb03c724e980316b8d0b50ee4c3415222ccc0a5
Jay Gohil <jaygextras@gmail.com>
2026-06-22 01:54:19 +0530

    Js tried do while loops on my own..

---
+++ src/Loops.java
@@ -0,0 +1,14 @@
+public class Loops {
+    public static void main(String[] args) {
+        //do while loops
+        int num = 1;
+        do {
+            System.out.println("first time kinda nervous");
+
+        }while(num<=0);
+
+        // so the diff betwn while nd do while is tht do while will run once no matterthe condn but while will check condn nd then run
+        //basically here conditn is while num<=0 but num is 1, technically the loop should not run but as its do while loop the do part runs once and thenn further the cond is evaluated!
+
+    }
+}
