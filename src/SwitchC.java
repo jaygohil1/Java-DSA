58c047445fa9cc7f8283b7cec676a991411d1265
Jay Gohil <jaygextras@gmail.com>
2026-06-23 13:59:02 +0530

    Jus simple stuff abt switch cases..

---
+++ src/SwitchC.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SwitchC {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number from 1 to 7 and you'll know \n whether its a weekdy or weekend");
+        int num = in.nextInt();
+
+//        switch (num) {
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//            default -> System.out.println("Enter a valid input");
+//        }
+
+
+        switch (num) {
+            case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
+            case 6, 7 -> System.out.println("Weekend");
+            default -> System.out.println("Invalid input");
+        }
+
+    }
+}
