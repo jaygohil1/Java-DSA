3d9c315b6a6f4144fd2a928e01d3c8f07a81c366
Jay Gohil <jaygextras@gmail.com>
2026-06-26 20:00:09 +0530

    Tried a string input program with methods.

---
+++ src/Meth_ods/StrExample.java
@@ -0,0 +1,24 @@
+package Meth_ods;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+import static Meth_ods.Sum.greet;
+
+public class StrExample {
+   public static void main(String[] args) {
+
+       String hello = message("Jay");
+       System.out.println("Hello, "+hello);
+
+//
+   }
+
+
+
+
+    static String message(String msg){
+        return msg;
+    }
+}
+
